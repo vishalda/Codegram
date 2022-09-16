306810863821782e32ea52792a788aebf6232962
vishal <vdavishu@gmail.com>
2022-09-16 15:46:58 +0530

    Delete post function added :tada:

--- api/post/urls.py
+++ api/post/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework import routers
 from rest_framework import generics
-from .views import createPost
+from . import views
 
 #router = routers.DefaultRouter()
 #router.register(r'',views.UserViewSet)
 
 
 urlpatterns = [
-    path('create-post/<int:userId>/',createPost,name='createPost'),
+    path('create-post/<int:userId>/',views.createPost,name='createPost'),
+    path('delete-post/<int:postId>/',views.deletePost,name='deletePost'),
 #    path('',include(router.urls)),
 ]
--- api/post/views.py
+++ api/post/views.py
@@ -1,3 +1,4 @@
+from socket import create_server
 from django.utils import timezone
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -18,9 +19,21 @@ def createPost(request,userId):
     CodeSnippet = request.POST['CodeSnippet'] if 'CodeSnippet' in request.POST.keys() else None
     CodeLanguage = request.POST['CodeLanguage'] if 'CodeLanguage' in request.POST.keys() else None 
     GroupId = request.POST['GroupId'] if 'GroupId' in request.POST.keys() else None 
+
     Created_at = timezone.now()
     author = get_object_or_404(User,pk=userId)
     group = get_object_or_404(pk=GroupId) if GroupId is not None else None
+
     instance = Post.objects.create(PostTitle=PostTitle,Description=Description,PostType=PostType,CodeBlock=CodeBlock,Image=Image,CodeSnippet=CodeSnippet,CodeLanguage=CodeLanguage,Created_at=Created_at,UserID=author,GroupId=group)
     instance.save()
     return JsonResponse({'success':'Post created successfully'})
+
+@csrf_exempt
+def deletePost(request,postId):
+    try:
+        instance = Post.objects.get(pk=postId)
+        instance.delete()
+
+        return JsonResponse({'success':'Deleted post successfully'})
+    except:
+        return JsonResponse({'error':'Post doesn\'t exist'})
