95959f04c18b5e08bdde6e4f4a3078beca53a4e1
vishal <vdavishu@gmail.com>
2022-09-19 23:33:06 +0530

    Group functions added :art:

---
+++ api/group/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Group
+
+class GroupListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields=('Title','Description','Image','admin')
